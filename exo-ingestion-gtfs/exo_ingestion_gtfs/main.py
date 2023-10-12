241687461c54643bf5baae592b2f8897dfb1d112
cricri777 <christian.bruneau777@gmail.com>
2023-10-12 19:08:17 -0400

    work in progress for ingestion

--- exo-ingestion-gtfs/exo_ingestion_gtfs/main.py
+++ exo-ingestion-gtfs/exo_ingestion_gtfs/main.py
@@ -1,7 +1,6 @@
 import fire
 import urllib.request as urllib
 import zipfile
-import csv
 
 from configuration import Configuration
 
@@ -10,18 +9,20 @@ def run(configuration_file_path="resources/configuration.yml"):
     config = Configuration(configuration_file_path)
     print(config.ingestion_dataset_trains)
     print(config.ingestion_dataset_autobuses)
+
     for train in config.ingestion_dataset_trains:
-        train_url = train["url"]
-        print(f"extract {train['name']}")
-        filehandle, _ = urllib.urlretrieve(train_url)
-        zip_file_object = zipfile.ZipFile(filehandle, 'r')
-        first_file = zip_file_object.namelist()[0]
-        file = zip_file_object.open(first_file)
-        content = file.read()
-        print("content")
-        print(content)
-        # with open(train["output_path"], "w") as f:
-        #     f.write(content.decode("utf-8"))
+        content = get_content(train["url"])
+        with open(train["output_path"], "w") as file:
+            file.write(content.decode())
+
+
+def get_content(url):
+    filehandle, _ = urllib.urlretrieve(url)
+    zip_file_object = zipfile.ZipFile(filehandle, 'r')
+    first_file = zip_file_object.namelist()[0]
+    file = zip_file_object.open(first_file)
+    content = file.read()
+    return content
 
 
 if __name__ == '__main__':
